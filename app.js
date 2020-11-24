cf5411c65d20d136298bc45f8a253d4b07bea4ab
mcodev <71014912+mcodev@users.noreply.github.com>
2020-11-24 11:03:57 +0200

    new

--- app.js
+++ app.js
@@ -23,21 +23,25 @@ for (i = 0; i < close.length; i++) {
 }
 
 
+
 // Add check class
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
         // Send to completed list
-        $('#myReady').append($('.checked'));
 
+        document.getElementById("myReady").appendChild(ev.target);
+          
+        // Run percentage
+        percentage();
 
     }
 }, false);
 
 
-// Create new list item
-function newElement() {
+// Create new list item using click
+document.getElementById("button").addEventListener("click", function newElement() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("taskInput").value;
     var t = document.createTextNode(inputValue);
@@ -48,6 +52,7 @@ function newElement() {
     } else {
         document.getElementById("myUL").appendChild(li);
 
+        percentage();
     }
     document.getElementById("taskInput").value = "";
 
@@ -63,17 +68,91 @@ function newElement() {
             var div = this.parentElement;
             div.style.display = "none";
             div.className = "closed";
+
+            // percentage();
+            percentage();
             
         }
     }
+});
+
+// Create new list item using enter key
+document.getElementById("taskInput").addEventListener("keyup", function newElement(event) {
+    
+    if (event.keyCode === 13) {      ////////////////////// prosoxi sto electron
+        event.preventDefault();
+        var li = document.createElement("li");
+        var inputValue = document.getElementById("taskInput").value;
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        if (inputValue === '') {
+            // document.getElementById('taskInput').style.color = "red";
+
+        } else {
+            document.getElementById("myUL").appendChild(li);
+
+            percentage();
+        }
+        document.getElementById("taskInput").value = "";
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                var div = this.parentElement;
+                div.style.display = "none";
+                div.className = "closed";
+
+                // percentage();
+                percentage();
+                
+            }
+        }
+    }
+});
+
+function percentage() {
+    const progress = document.querySelector('.progress-done');
+    let output = document.getElementById('number');
+    let lisIn = document.getElementById("myUL").getElementsByTagName("LI");
+    let lisReady = document.getElementById("myReady").getElementsByTagName("LI");
+    let closed1 = document.getElementById("myReady").getElementsByClassName("closed");
+    let closed2 = document.getElementsByClassName("closed");
+    let paronomastis = lisIn.length + lisReady.length - closed2.length;
+    let arithmitis = lisReady.length - closed1.length;
+    var final = parseInt((arithmitis / paronomastis) * 100);
+
+
+
+    if (arithmitis == 0 && paronomastis == 0) {
+        final = 0;
+        output.innerHTML = `${final}%`;  
+
+        progress.setAttribute('data-done', final);
+        progress.style.width = progress.getAttribute('data-done') + '%';
+        progress.style.opacity = 1;
+
+    } else if (arithmitis < 0) {
+        arithmitis = 0;
+        var final = parseInt((arithmitis / paronomastis) * 100);
+
+        progress.setAttribute('data-done', final);
+        progress.style.width = progress.getAttribute('data-done') + '%';
+        progress.style.opacity = 1;
+
+    } else {
+        var final = parseInt((arithmitis / paronomastis) * 100);
+        output.innerHTML = `${final}%`;
+       
+        progress.setAttribute('data-done', final);
+        progress.style.width = progress.getAttribute('data-done') + '%';
+        progress.style.opacity = 1;
+    }
+
 }
 
-
-var today = new Date();
-
-var myVar=setInterval(function(){myTimer()},1000);
-
-function myTimer() {
-    var d = new Date();   
-    document.getElementById("date").innerHTML = d.toLocaleTimeString();
-}
