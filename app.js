07e5d2ff91144d55bcb8ef9dfb0affd32cf4492d
mcodev <71014912+mcodev@users.noreply.github.com>
2020-11-27 01:56:57 +0200

    new

--- app.js
+++ app.js
@@ -1,3 +1,80 @@
+// top manager selector
+let pm =document.getElementById("pm");
+let tm =document.getElementById("tm");
+
+var projectManager = document.getElementById("projectManager");
+var taskManager = document.getElementById("taskManager");
+
+
+pm.addEventListener("click",function (){
+    if (projectManager.style.display === "none") {
+        projectManager.style.display = "flex";
+        pm.classList.add("checked2");
+        tm.classList.remove("checked2");
+        taskManager.style.display = "none";
+      } 
+} );
+
+tm.addEventListener("click",function (){
+    if (taskManager.style.display === "none") {
+        taskManager.style.display = "flex";
+        tm.classList.add("checked2");
+        pm.classList.remove("checked2");
+        projectManager.style.display = "none";
+      }
+} );
+
+//CLock
+
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+const month = document.querySelector('.month');
+const day = document.querySelector('.day');
+const year = document.querySelector('.year');
+
+function setDate() {
+	const now = new Date();
+	const mm = now.getMonth();
+	const dd = now.getDate();
+	const yyyy = now.getFullYear();
+	const secs = now.getSeconds();
+	const mins = now.getMinutes();
+	const hrs = now.getHours();
+	const monthName = [
+		'January','February','March','April',
+		'May','June','July','August','September',
+		'October','November','December'
+	];
+	
+	if (hrs > 12) {
+		hours.innerHTML = hrs - 12;
+	} else {
+		hours.innerHTML = hrs;
+	}
+	
+	if (secs < 10) {
+		seconds.innerHTML = '0' + secs;
+	} else {
+		seconds.innerHTML = secs;
+	}
+	
+	if (mins < 10) {
+		minutes.innerHTML = '0' + mins;
+	} else {
+		minutes.innerHTML = mins;
+	}
+	
+	month.innerHTML = monthName[mm];
+	day.innerHTML = dd;
+	year.innerHTML = yyyy;
+}
+
+setInterval(setDate,1000);
+
+////////////////////////////////////////////INPUT
+
 // Create close button
 var myNodelist = document.getElementsByTagName("LI");
 var i;
@@ -40,41 +117,6 @@ list.addEventListener('click', function(ev) {
 }, false);
 
 
-// Create new list item using click
-document.getElementById("button").addEventListener("click", function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("taskInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        // document.getElementById('taskInput').style.color = "red";
-
-    } else {
-        document.getElementById("myUL").appendChild(li);
-
-        percentage();
-    }
-    document.getElementById("taskInput").value = "";
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-            div.className = "closed";
-
-            // percentage();
-            percentage();
-            
-        }
-    }
-});
 
 // Create new list item using enter key
 document.getElementById("taskInput").addEventListener("keyup", function newElement(event) {
@@ -155,71 +197,5 @@ function percentage() {
         progress.style.width = progress.getAttribute('data-done') + '%';
         progress.style.opacity = 1;
     }
-
 }
 
-
-// Timer
-
-var timer = null;
-var duration = null;
-
-var interval = setInterval(function () {
-  
-    hours = parseInt((timer /3600)%24, 10);
-    minutes = parseInt((timer / 60)%60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    displayH.textContent = hours;
-    displayS.textContent = seconds;
-    displayM.textContent = minutes;
-
-    if (--timer < 0) {
-        timer = null;
-        clearInterval(interval);
-    }
-}, 1000);
-
-function startTimer(duration, displayH, displayM , displayS) {
-
-    clearInterval(timer);    
-    timer = null;
-    
-    timer = duration, hours, minutes, seconds;
-
-    
-
-}
-
-function count(e) {
-    duration = e * 60 * 60;
-    displayH = document.querySelector('#hours');
-    displayM = document.querySelector('#minutes');
-    displayS = document.querySelector('#seconds');
-    startTimer(duration, displayH , displayM, displayS);
-};
-
-document.getElementById("CountBtn2").addEventListener("click", function(){ count(2);});
-document.getElementById("CountBtn4").addEventListener("click", function(){ count(4);});
-document.getElementById("CountBtn8").addEventListener("click", function(){ count(8);});
-
-// document.querySelector('#clock').addEventListener("click", function(){ 
-//     clearTimeout(0);
-//     clearInterval(timer);    
-//     timer = null;
-// });
-
-
-//Clock
-var today = new Date();
-
-var myVar=setInterval(function(){myTimer()},1000);
-
-function myTimer() {
-    var d = new Date();   
-    document.getElementById("date").innerHTML = d.toLocaleTimeString();
-}
